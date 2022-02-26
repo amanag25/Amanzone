53a37ef3b803a793ee13abb9a218204c11bb96ce
aman <amanvivoy51l@gmail.com>
2022-02-26 19:26:40 +0530

    first git commit

--- amanzone/settings.py
+++ amanzone/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-%@pbuy3^mg^kkot98v(xgx_)i+b+w0lho-bh4t@+=kxt&pipzc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://amanzone001.herokuapp.com/","localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.Whitenoise',
+    # 'whitenoise.middleware.Whitenoise',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
